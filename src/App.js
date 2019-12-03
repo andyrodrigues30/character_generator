6f15c51ae3b3a14e0d988c71bf3bfef98e6e1332
Andrea Rodrigues <andrea.melissa.rodrigues@gmail.com>
2019-12-03 21:44:26 +0000

    clean up files

--- src/App.js
+++ src/App.js
@@ -3,27 +3,18 @@ import './App.css';
 
 const json = require('./data.json');
 
-const personality = json["personality"];
-const character = json["character"];
-const character2 = json["character2"];
-const clothing = json["clothing"];
-const verb = json["verb"];
-const holdingitem = json["holdingitem"];
-const ridingitem = json["ridingitem"];
-const playingitem = json["playingitem"];
-
 function getItem() {
-  if (verb === 'holding') {
-    let item = holdingitem[Math.floor(Math.random() * holdingitem.length)];
+  if (json["verb"] === 'holding') {
+    let item = json["holdingitem"][Math.floor(Math.random() * json["holdingitem"].length)];
     return item
-  } else if (verb === 'riding') {
-    let item = ridingitem[Math.floor(Math.random() * ridingitem.length)];
+  } else if (json["verb"] === 'riding') {
+    let item = json["ridingitem"][Math.floor(Math.random() * json["ridingitem"].length)];
     return item
-  } else if (verb === 'playing') {
-    let item = playingitem[Math.floor(Math.random() * playingitem.length)];
+  } else if (json["verb"] === 'playing') {
+    let item = json["playingitem"][Math.floor(Math.random() * json["playingitem"].length)];
     return item
   } else {
-    let item = character2[Math.floor(Math.random() * character2.length)];
+    let item = json["character2"][Math.floor(Math.random() * json["character2"].length)];
     return item
   }
 }
@@ -34,7 +25,7 @@ class App extends React.Component {
   }
 
   onClickGenerate = () => {
-    const sentence = `${personality[Math.floor(Math.random() * personality.length)]} ${character[Math.floor(Math.random() * character.length)]} wearing ${clothing[Math.floor(Math.random() * clothing.length)]} ${verb[Math.floor(Math.random() * verb.length)]} ${getItem()}.`;
+    const sentence = `${json["personality"][Math.floor(Math.random() * json["personality"].length)]} ${json["character"][Math.floor(Math.random() * json["character"].length)]} wearing ${json["clothing"][Math.floor(Math.random() * json["clothing"].length)]} ${json["verb"][Math.floor(Math.random() * json["verb"].length)]} ${getItem()}.`;
     this.setState({
       text: sentence
     });
@@ -45,7 +36,7 @@ class App extends React.Component {
       <div className="App">
         <h1>Character Generator</h1>
         <button onClick={this.onClickGenerate}>Generate</button>
-        <h2>{this.state.text}</h2>
+        <p>{this.state.text}</p>
       </div>
     );
   }
